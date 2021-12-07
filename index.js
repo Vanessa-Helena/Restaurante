ba84e891ed606836dab5ec4437d8bb855c06931b
Vanessa-Helena <vanesa-lima10@hotmail.com>
2021-12-07 18:09:34 -0300

    apresentando uduario individual

--- index.js
+++ index.js
@@ -11,18 +11,22 @@ app.set('views', path.join(__dirname, 'views'));
 
 let pessoas = [
     {
+        id: 1,
         nome: "Vanessa",
         registro: 123
     },
     {
+        id: 2,
         nome: "Carlos",
         registro: 456
     },
     {
+        id: 3,
         nome: "Tomoyo",
         registro: 789
     },
     {
+        id: 4,
         nome: "Shiryu",
         registro: 101
     }
@@ -38,6 +42,12 @@ app.get('/pessoas', (req, res) => {
 
 app.get('/pessoas/novo', (req, res) => {
     res.render('pessoas/novo');
+});
+
+app.get('/pessoas/:id', (req, res) => {
+    const {id} = req.params;
+    const pessoa = pessoas.find(p=> p.id === id);
+    res.render('pessoas/apresenta', {pessoa});
 });
 
 app.post('/pessoas', (req, res) => {
