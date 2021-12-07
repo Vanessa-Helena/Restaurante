6dc7e3586bd62de3959ca82b5433398eef94eb1f
Vanessa-Helena <vanesa-lima10@hotmail.com>
2021-12-07 16:34:27 -0300

    crinado rota pessoa

--- index.js
+++ index.js
@@ -5,8 +5,31 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+let pessoas = [
+    {
+        nome: "Vanessa",
+        registro: 123
+    },
+    {
+        nome: "Carlos",
+        registro: 456
+    },
+    {
+        nome: "Tomoyo",
+        registro: 789
+    },
+    {
+        nome: "Shiryu",
+        registro: 101
+    }
+] 
+
 app.get('/', (req, res) => {
     res.render('home')
+});
+
+app.get('/pessoa', (req, res) => {
+    res.render('pessoas/index')
 });
 
 app.listen(3000, ()=>{
