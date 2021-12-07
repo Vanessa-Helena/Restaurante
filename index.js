e7b7fc7b681e6c32f47a25b2143b1eddc7dfb97b
Vanessa-Helena <vanesa-lima10@hotmail.com>
2021-12-07 18:41:09 -0300

    inclusao do method overrid no index

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const methodOverride = require('method-override');
 
 app.use(express.urlencoded({extended:true}));
 
 app.use(express.json());
+
+app.use(methodOverride('__method'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
