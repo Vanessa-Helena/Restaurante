8f36fbe28274acfa6d3e3ed5c7c117b5e0495b9d
Vanessa-Helena <vanesa-lima10@hotmail.com>
2021-12-08 21:52:46 -0300

    migration pessoa

---
+++ migrations/20211208002854-create-pessoa.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const sequelize = require("sequelize");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    
+    await queryInterface.createTable('pessoa', { 
+      id: {
+        type: Sequelize.INTEGER,
+        pimarykey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      registro: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        type: sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('NOW')
+      },
+      updatedAt: {
+        type: sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn('NOW')
+      }
+
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('pessoa');
+  },
+};
