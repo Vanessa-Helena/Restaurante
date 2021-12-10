eea7a690fca5817ee47054f2f175f2655367e683
Vanessa-Helena <vanesa-lima10@hotmail.com>
2021-12-10 20:38:34 -0300

    adicionando migretions e seeds de user

---
+++ controllers/usersController.js
@@ -0,0 +1,64 @@
+const {Pessoa, User} = require('../models');
+const {Router} = require('express');
+
+const roteador = Router();
+
+roteador.get('/', async(req, res)=>{
+
+});
+
+roteador.get('/login', (req, res)=>{
+    res.render('usuarios/login');
+});
+
+roteador.get('/logoff', (req, res)=>{
+    req.session.destroy();
+    res.redirect('/usuarios/login');
+});
+
+roteador.get('/novo', (req, res)=>{
+    res.render('usuarios/novo');
+});
+
+roteador.get('/:id', async(req, res)=>{
+
+});
+
+roteador.get('/:id/edit', async(req, res)=>{
+});
+
+roteador.post('/login', async (req, res)=>{
+    const {nome, senha} = req.body;
+    const user = await User.findOne({
+        where: {
+            nome: nome,
+            senha: senha
+        }
+    });
+
+    req.session.login = false;
+    if(user){
+        req.session.login = true;
+        res.redirect('/pessoas');
+    }else{
+        res.redirect('/usuarios/login');
+    }
+});
+
+roteador.post('/novo', async (req, res)=>{
+    const {username, password} = req.body;
+    await User.create({username, password});
+    res.redirect('/usuarios/login');
+});
+
+
+roteador.patch('/:id', async(req, res)=>{
+
+});
+
+roteador.delete('/:id', async(req, res)=>{
+
+ 
+});
+
+module.exports = roteador;
